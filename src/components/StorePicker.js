6a71f3c843b14b14681b9b3ad26abc138738a95b
Luis Fernando Galarcio Marín <lgalarcio@NET2.NET2>
2019-12-27 16:43:17 -0500

    first component imported

---
+++ src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>I am the StorePicker!</p>
+    }
+}
+
+export default StorePicker
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom'; // Rendering react for websites different than for mobiles devices
+                                    // {} to import one of the methods of the package
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker />, document.querySelector('#main'));
+// render(<p>HEEEYYYYYYY</p>, document.querySelector('#main'));
