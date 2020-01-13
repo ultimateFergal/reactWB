c0cf43983676cc6c76a56c32159ede65adfc8f0e
Luis Fernando Galarcio Marín <lgalarcio@NET2.NET2>
2020-01-13 15:30:42 -0500

    header, inventory and order component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am the StorePicker!</p>
+        // return <p>I am the StorePicker!</p>
+        return ( // Mandatory parenthesis si it does not explode
+            <>{/* <React.Fragment> */}{/* // only needed when using more sobling elements and not just one parent element */}
+            <form className="store-selector"> {/* This is JSX */}
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store</button>
+            </form>
+            </>/* </React.Fragment> */
+
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from 'react-dom'; // Rendering react for websites different than for mobiles devices
                                     // {} to import one of the methods of the package
 import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css';
 
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
+// render(<StorePicker />, document.querySelector('#main'));
 // render(<p>HEEEYYYYYYY</p>, document.querySelector('#main'));
